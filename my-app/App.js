230856e806a51f4bf8f9558eb5ad2b5e6845de24
Juhaied <juhaied.hossen@northsouth.edu>
2023-08-28 15:13:20 +0600

    Use State

--- my-app/App.js
+++ my-app/App.js
@@ -1,17 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
 export default function App() {
+  
+
+  const [name,setName] =useState('Juhaied');
+  const [person,setPerson] = useState({name : 'Juhaied Hossen',age:24});
+  const clickHandler = () =>{
+    setName('Nabid');
+  }
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.boldText}>Hello World</Text>
-      </View>
-
-      <View style={styles.body}>
-        <Text style={styles.boldText}>Blah Blah Blah</Text>
-        <Text>Blah Blah Blah</Text>
-        <Text>Blah Blah Blah</Text>
+      <Text>My Name is {name}</Text>
+      <Text>His name is {person.name} and he is {person.age} years old</Text>
+      <View style={styles.buttonContainer}>
+        <Button title='Update Name' onPress={clickHandler}/>
       </View>
     </View>
   );
@@ -34,6 +37,10 @@ const styles = StyleSheet.create({
   body:{
     backgroundColor:'yellow',
     padding:20,
-    }
+  },
+  buttonContainer :{
+    marginTop:30,
+    backgroundColor:'black'
+  }
   
 });
