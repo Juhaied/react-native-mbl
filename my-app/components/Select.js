6296adea5aef5c2e0371d1209c6e14e1e196d321
Juhaied <juhaied.hossen@northsouth.edu>
2023-09-05 20:28:22 +0600

    1st Page Completly Done

--- my-app/components/Select.js
+++ my-app/components/Select.js
@@ -2,83 +2,97 @@ import React, { useState } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
+export default function Select() {
+  const genders = [
+    { id: 'male', label: 'Male', icon: 'mars', color: 'blue' },
+    { id: 'female', label: 'Female', icon: 'venus', color: 'pink' },
+  ];
 
-export default function Select(){
-    const [selectedGender, setSelectedGender] = useState(null);
+  const [selectedGender, setSelectedGender] = useState(null);
 
-    const handleGenderSelect = (gender) => {
-        setSelectedGender(gender);
-    };
+  const handleGenderSelect = (genderId) => {
+    setSelectedGender(genderId);
+  };
 
-    return (
-        <View style={styles.container}>
-          <Text style={styles.header}>Select Gender</Text>
-          <View style={styles.line} />
-    
-          <View style={styles.genderContainer}>
-            <TouchableOpacity
-              style={[
-                styles.genderBox,
-                selectedGender === 'male' && styles.selectedGender,
-              ]}
-              onPress={() => handleGenderSelect('male')}
-            >
-              <Icon name="mars" size={30} color="blue" />
-              <Text style={styles.genderText}>Male</Text>
-            </TouchableOpacity>
-            <View style={styles.gap} />
-            <TouchableOpacity
-              style={[
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>Select Gender</Text>
+      <View style={styles.line} />
+
+      <View style={styles.genderContainer}>
+        {genders.map((gender, index) => (
+          <TouchableOpacity
+            key={gender.id}
+            style={[
               styles.genderBox,
-              selectedGender === 'female' && styles.selectedGender,
-          ]}
-          onPress={() => handleGenderSelect('female')}
-        >
-          <Icon name="venus" size={30} color="pink" />
-          <Text style={styles.genderText}>Female</Text>
-        </TouchableOpacity>
+              selectedGender === gender.id && styles.selectedGender,
+              index !== 0 && { marginLeft: 20 }, // Add a left margin to create a gap
+            ]}
+            onPress={() => handleGenderSelect(gender.id)}
+          >
+            <Icon name={gender.icon} size={40} color={gender.color} />
+            <Text style={styles.genderText}>{gender.label}</Text>
+            <View style={styles.radioButton}>
+              {selectedGender === gender.id && (
+                <View style={styles.radioButtonInner} />
+              )}
+            </View>
+          </TouchableOpacity>
+        ))}
       </View>
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
-    container: {
-      alignItems: 'center',
-      paddingTop: 20,
-    },
-    header: {
-      fontSize: 24,
-      fontWeight: 'bold',
-      marginBottom: 10,
-    },
-    line: {
-      width: '80%',
-      borderBottomWidth: 1,
-      borderBottomColor: '#ccc',
-      marginBottom: 20,
-    },
-    genderContainer: {
-      flexDirection: 'row',
-      justifyContent: 'space-around',
-      marginBottom: 20,
-    },
-    genderBox: {
-      alignItems: 'center',
-      borderWidth: 2,
-      borderColor: '#ccc',
-      borderRadius: 8,
-      padding: 30,
-      
-    },
-    gap: {
-      width: 40, // Adjust the width to control the size of the gap
-    },
-    selectedGender: {
-      borderColor: 'green', // Any styling for selected options
-    },
-    genderText: {
-      fontSize: 16,
-      marginTop: 5,
-    },
-  });
+  container: {
+    alignItems: 'center',
+    paddingTop: 20,
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  line: {
+    width: '80%',
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    marginBottom: 20,
+  },
+  genderContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginBottom: 20,
+  },
+  genderBox: {
+    alignItems: 'center',
+    borderWidth: 2,
+    borderColor: '#ccc',
+    borderRadius: 10, // Increase the border radius for larger boxes
+    padding: 20,
+    minWidth: 110, // Minimum width for each box
+    flexDirection: 'column', // Stack content vertically
+  },
+  radioButton: {
+    width: 20,
+    height: 20,
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: '#ccc',
+    marginTop: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  radioButtonInner: {
+    width: 10,
+    height: 10,
+    borderRadius: 5,
+    backgroundColor: '#40e0d0',
+  },
+  
+  genderText: {
+    fontSize: 22,
+    marginTop: 5,
+  },
+});
